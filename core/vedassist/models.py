e8d2b7cd8262c3c6464a02c68a525ca27204f751
Vijay Varadarajan <vveejaay11@yahoo.com>
2023-06-06 17:11:46 +0530

    tiny changes

--- core/vedassist/models.py
+++ core/vedassist/models.py
@@ -3,30 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-
-class AyurvedicPrediction(models.Model):
-    cold = models.IntegerField()
-    eyepain = models.IntegerField()
-    fever = models.IntegerField()
-    headache = models.IntegerField()
-    stomachache = models.IntegerField()
-    dizziness = models.IntegerField()
-    vomiting = models.IntegerField()
-    chestpain = models.IntegerField()
-    jointpain = models.IntegerField()
-    loosemotion = models.IntegerField()
-    throatinfection = models.IntegerField()
-    age = models.IntegerField()
-    gender = models.IntegerField()
-    weight = models.IntegerField()
-    medicine1 = models.CharField(max_length=100)
-    medicine2 = models.CharField(max_length=100)
-    medicine3 = models.CharField(max_length=100)
-
-
-
-
 class Medicine(models.Model):
 
     user =models.ForeignKey(User , on_delete=models.SET_NULL , null=True,blank=True)
